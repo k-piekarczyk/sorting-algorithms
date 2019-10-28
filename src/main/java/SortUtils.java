6d4c94e9fae13a20c0300365588c37313568f352
Krzysiek Piekarczyk <krzys@piekarczyk.eu>
2019-10-28 16:58:24 +0100

    add sort utility class

---
+++ src/main/java/SortUtils.java
@@ -0,0 +1,11 @@
+public class SortUtils {
+    public static void swap(double[] source, int index1, int index2) {
+        if (index1 >= source.length - 1 || index2 >= source.length - 1 || index1 < 0 || index2 < 0)
+            throw new IndexOutOfBoundsException();
+        else if (index1 == index2) return;
+
+        double temp = source[index1];
+        source[index1] = source[index2];
+        source[index2] = temp;
+    }
+}
